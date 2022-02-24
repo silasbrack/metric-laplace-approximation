53cbd1fb8df6a4d28b246dd04358ae10a52c1dae
Tobias Jessen <mail@tobijes.com>
2022-02-24 10:41:56 +0100

    make dataset

--- src/data/make_dataset.py
+++ src/data/make_dataset.py
@@ -4,15 +4,15 @@ import os
 
 import torch
 from torchvision import datasets, transforms
-from pl_bolts import CIFAR10DataModule
+from pl_bolts.datamodules import CIFAR10DataModule
 
-@click.command()
-@click.argument('input_filepath', type=click.Path(exists=True))
 def main(input_filepath):
     """ Runs data processing scripts to turn raw data from (../raw) into
         cleaned data ready to be analyzed (saved in ../processed).
     """
     cifar10 = CIFAR10DataModule(input_filepath)
+    cifar10.prepare_data()
+    print(next(cifar10.train_dataloader()))
 
 if __name__ == "__main__":
     main("cifar10")
