ee695b8ff8d0c20f329fc2a5a7732d97adc97842
Silas Brack <s174433@student.dtu.dk>
2022-03-02 11:48:28 +0100

    Added experiments.

---
+++ experiments/run.sh
@@ -0,0 +1,39 @@
+#!/bin/sh
+### General options
+### –- specify queue: gpuv100, gputitanxpascal, gpuk40, gpum2050 --
+#BSUB -q gpuv100
+### -- set the job Name --
+#BSUB -J silas
+### -- ask for number of cores (default: 1) --
+#BSUB -n 1
+### -- Select the resources: 1 gpu in exclusive process mode --
+#BSUB -gpu "num=1:mode=exclusive_process"
+### -- set walltime limit: hh:mm --  maximum 24 hours for GPU-queues right now
+#BSUB -W 01:00
+### -- request 5GB of memory
+#BSUB -R "rusage[mem=5GB]"
+### -- set the email address --
+# please uncomment the following line and put in your e-mail address,
+# if you want to receive e-mail notifications on a non-default address
+###BSUB -u s174433@student.dtu.dk
+### -- send notification at start --
+#BSUB -B
+### -- send notification at completion--
+#BSUB -N
+### -- Specify the output and error file. %J is the job-id --
+### -- -o and -e mean append, -oo and -eo mean overwrite --
+##BSUB -o silas-%J.out
+##BSUB -e silas_%J.err
+# -- end of LSF options --
+
+# Exits if any errors occur at any point (non-zero exit code)
+set -e
+
+# Load modules
+module load python3/3.9.6
+module load cuda/11.3
+
+# Load virtual Python environment
+. ./venv/bin/activate
+
+python -c "import torch; print(torch.__version__);"
