06132280ed1ceef27d3411441a897f052fb37c8b
Silas Brack <s174433@student.dtu.dk>
2022-03-20 19:27:56 +0100

    Cleaned up toy example.

--- src/toy_example.py
+++ src/toy_example.py
@@ -1,4 +1,3 @@
-import numpy as np
 import torch
 from asdfghjkl.gradient import batch_gradient
 from laplace import Laplace
@@ -36,6 +35,8 @@ def jacobians(x, model, output_size=784):
 
 
 num_observations = 1000
+num_outputs = 1
+
 X = torch.rand((num_observations, 1)).float()
 y = 4.5 * torch.cos(2 * torch.pi * X + 1.5 * torch.pi) - \
     3 * torch.sin(4.3 * torch.pi * X + 0.3 * torch.pi) + \
@@ -45,7 +46,6 @@ dataset = TensorDataset(X, y)
 dataloader = DataLoader(dataset, batch_size=num_observations)
 x, y = next(iter(dataloader))
 
-num_outputs = 1
 model = nn.Sequential(
     nn.Linear(1, 16),
     nn.ReLU(),
@@ -65,15 +65,16 @@ for hessian_structure in ["diag", "full"]:
     la.fit(dataloader)
     la.optimize_prior_precision()
 
-    Js, f = jacobians(x, model, output_size=1)
+    Js, f = jacobians(x, model, output_size=num_outputs)
     assert Js.shape == (num_observations, num_outputs, num_params)
 
+    Hs = torch.einsum("nij,nkl->njl", Js, Js)
+    assert Hs.shape == (num_observations, num_params, num_params)
+
     if hessian_structure == "diag":
-        Hs = torch.einsum("nij,nij->nj", Js, Js)
+        # Hs = torch.einsum("nij,nij->nj", Js, Js)
+        Hs = torch.einsum("nii->ni", Hs)
         assert Hs.shape == (num_observations, num_params)
-    elif hessian_structure == "full":
-        Hs = torch.einsum("nij,nkl->njl", Js, Js)
-        assert Hs.shape == (num_observations, num_params, num_params)
 
     # Hs.sum(dim=0), since we sum over the observations (that's what they seem to do)
     torch.testing.assert_close(la.H, Hs.sum(dim=0), rtol=1e-5, atol=0.)  # Less than 0.001% off
