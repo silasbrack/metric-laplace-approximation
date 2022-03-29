1f982421b6924989b4665c206a1c8e475bc70df5
Silas Brack <s174433@student.dtu.dk>
2022-03-29 08:15:59 +0200

    Implemented layer-wise toy Hessian RMSE calculation

--- src/toy_example.py
+++ src/toy_example.py
@@ -1,9 +1,65 @@
+import time
+
 import torch
 from laplace import Laplace
 from torch import nn
 from torch.utils.data import DataLoader, TensorDataset
 
 from src.models.manual_hessian import RmseHessianCalculator
+
+
+def compute_hessian(x, feature_maps, net, output_size, h_scale):
+    H = []
+    bs = x.shape[0]
+    feature_maps = [x] + feature_maps
+    tmp = torch.diag_embed(torch.ones(bs, output_size, device=x.device))
+
+    with torch.no_grad():
+        for k in range(len(net) - 1, -1, -1):
+
+            # compute Jacobian wrt input
+            if isinstance(net[k], torch.nn.Linear):
+                diag_elements = torch.diagonal(tmp, dim1=1, dim2=2)
+                feature_map_k2 = (feature_maps[k] ** 2).unsqueeze(1)
+
+                h_k = torch.bmm(diag_elements.unsqueeze(2), feature_map_k2).view(bs, -1)
+
+                # has a bias
+                if net[k].bias is not None:
+                    h_k = torch.cat([h_k, diag_elements], dim=1)
+
+                H = [h_k] + H
+
+            # compute Jacobian wrt input
+            if isinstance(net[k], torch.nn.Tanh):
+                J_tanh = torch.diag_embed(
+                    torch.ones(feature_maps[k + 1].shape, device=x.device)
+                    - feature_maps[k + 1] ** 2
+                )
+                tmp = torch.einsum("bnm,bnj,bjk->bmk", J_tanh, tmp, J_tanh)
+            elif isinstance(net[k], torch.nn.ReLU):
+                J_relu = torch.diag_embed(
+                    (feature_maps[k] > 0).float()
+                )
+                tmp = torch.einsum("bnm,bnj,bjk->bmk", J_relu, tmp, J_relu)
+
+            if k == 0:
+                break
+
+            # compute Jacobian wrt weight
+            if isinstance(net[k], torch.nn.Linear):
+                tmp = torch.einsum("nm,bnj,jk->bmk",
+                                   net[k].weight,
+                                   tmp,
+                                   net[k].weight)
+
+    H = torch.cat(H, dim=1)
+
+    # mean over batch size scaled by the size of the dataset
+    H = h_scale * torch.mean(H, dim=0)
+
+    return H
+
 
 num_observations = 1000
 
@@ -21,6 +77,16 @@ model = nn.Sequential(
     nn.ReLU(),
     nn.Linear(16, 16),
     nn.ReLU(),
+    nn.Linear(16, 32),
+    nn.ReLU(),
+    nn.Linear(32, 64),
+    nn.ReLU(),
+    nn.Linear(64, 32),
+    nn.ReLU(),
+    nn.Linear(32, 16),
+    nn.ReLU(),
+    nn.Linear(16, 16),
+    nn.ReLU(),
     nn.Linear(16, 1)
 )
 num_params = sum(p.numel() for p in model.parameters())
@@ -32,16 +98,35 @@ for hessian_structure in ["diag", "full"]:
         hessian_structure=hessian_structure,
         subset_of_weights="all",
     )
+    t0 = time.perf_counter()
     la.fit(dataloader)
+    elapsed_la = time.perf_counter() - t0
 
+    t0 = time.perf_counter()
     Hs = RmseHessianCalculator().calculate_hessian(
         x,
         model=model,
         num_outputs=1,
         hessian_structure=hessian_structure,
     )
+    elapsed_rmse = time.perf_counter() - t0
+    torch.testing.assert_close(la.H, Hs, rtol=1e-4, atol=0.)  # Less than 0.01% off
 
-    torch.testing.assert_close(la.H, Hs, rtol=1e-5, atol=0.)  # Less than 0.001% off
+    activation = []
+    def get_activation():
+        def hook(model, input, output):
+            activation.append(output.detach())
+        return hook
+    for layer in model:
+        layer.register_forward_hook(get_activation())
+    output = model(x)
+
+    t0 = time.perf_counter()
+    Hs = compute_hessian(x, activation, model, output_size=1,
+                         h_scale=num_observations)
+    elapsed = time.perf_counter() - t0
+
+    torch.testing.assert_close(la.H, Hs, rtol=1e-2, atol=0.)  # Less than 1% off
 
     # Prior precision is one.
     if hessian_structure == "diag":
