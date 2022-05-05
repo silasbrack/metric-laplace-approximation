9da3333bdeaa62513e76d2b9617cef0d5138118e
Silas Brack <s174433@student.dtu.dk>
2022-05-05 07:33:42 +0200

    Add more datasets.

---
+++ src/data/__init__.py
@@ -0,0 +1,2 @@
+from .cifar100 import CIFAR100DataModule
+from .svhn import SVHNDataModule
--- src/post_hoc_laplace.py
+++ src/post_hoc_laplace.py
@@ -13,30 +13,52 @@ from torch import nn
 from torch.utils.data import Subset, DataLoader
 from torch.nn.utils import parameters_to_vector, vector_to_parameters
 from torch.optim import Adam
-from pl_bolts.datamodules import CIFAR10DataModule
+from pl_bolts.datamodules import (
+    CIFAR10DataModule,
+    FashionMNISTDataModule,
+    MNISTDataModule,
+)
 
-from src.data.cifar100 import CIFAR100DataModule
+from src.data import CIFAR100DataModule, SVHNDataModule
 from src.hessian.layerwise import ContrastiveHessianCalculator
 from src.models.utils import test_model
 
 
 class ConvDense(nn.Module):
-    def __init__(self, latent_dim):
+    def __init__(self, channels, latent_dim):
         super().__init__()
-        self.conv = nn.Sequential(
-            nn.Conv2d(3, 16, 3, 1),
-            nn.ReLU(),
-            nn.Conv2d(16, 32, 3, 1),
-            nn.ReLU(),
-            nn.MaxPool2d(2),
-            nn.Dropout2d(0.25),
-            nn.Flatten(),
-        )
-        self.linear = nn.Sequential(
-            nn.Linear(6272, 64),
-            nn.ReLU(),
-            nn.Linear(64, latent_dim),
-        )
+        if channels == 1:
+            self.conv = nn.Sequential(
+                nn.Conv2d(1, 16, 3, 1),
+                nn.ReLU(),
+                nn.Conv2d(16, 32, 3, 1),
+                nn.ReLU(),
+                nn.MaxPool2d(2),
+                nn.Dropout2d(0.25),
+                nn.Flatten(),
+            )
+            self.linear = nn.Sequential(
+                nn.Linear(4608, 64),
+                nn.ReLU(),
+                nn.Linear(64, latent_dim),
+            )
+        elif channels == 3:
+            self.conv = nn.Sequential(
+                nn.Conv2d(3, 16, 3, 1),
+                nn.ReLU(),
+                nn.Conv2d(16, 32, 3, 1),
+                nn.ReLU(),
+                nn.MaxPool2d(2),
+                nn.Dropout2d(0.25),
+                nn.Flatten(),
+            )
+            self.linear = nn.Sequential(
+                nn.Linear(6272, 64),
+                nn.ReLU(),
+                nn.Linear(64, latent_dim),
+            )
+        else:
+            raise NotImplementedError
 
     def forward(self, x):
         x = self.conv(x)
@@ -57,26 +79,26 @@ class AllPermutationsMiner:
 
 def run():
     device = "cuda" if torch.cuda.is_available() else "cpu"
+    # id_ = "fashionmnist"
+    # ood = "mnist"
+    id_ = "cifar10"
+    ood = "svhn"
 
     latent_dim = 25
-    net = ConvDense(latent_dim).to(device)
-    # net = nn.Sequential(
-    #     nn.Flatten(),
-    #     nn.Linear(28*28*1, 32),
-    #     nn.ReLU(),
-    #     nn.Linear(32, 64),
-    #     nn.ReLU(),
-    #     nn.Linear(64, 32),
-    #     nn.ReLU(),
-    #     nn.Linear(32, latent_dim),
-    # ).to(device)
-    num_params = sum(p.numel() for p in net.parameters())
-    logging.info(f"Model has {num_params} parameters.")
+    if id_ == "fashionmnist":
+        net = ConvDense(1, latent_dim).to(device)
+        data = FashionMNISTDataModule("data/", 16, 4)
+        data.prepare_data()
+        data.setup()
+    elif id_ == "cifar10":
+        net = ConvDense(3, latent_dim).to(device)
+        data = CIFAR10DataModule("data/", 16, 4)
+        data.setup()
+    else:
+        raise NotImplementedError
+    # num_params = sum(p.numel() for p in net.parameters())
+    # logging.info(f"Model has {num_params} parameters.")
 
-    # data = CIFARData("data/", 16, 4)
-    # data.setup()
-    data = CIFAR10DataModule("data/", 16, 4)
-    data.setup()
     loader = data.train_dataloader()
 
     # accuracy = test_model(
@@ -87,7 +109,7 @@ def run():
     # )
 
     logging.info("Finding MAP solution.")
-    epochs = 100
+    epochs = 50
     lr = 3e-4
     miner = miners.MultiSimilarityMiner()
     contrastive_loss = losses.ContrastiveLoss()
@@ -129,15 +151,11 @@ def run():
         h.append(hessian)
         # h.append(compute_hessian(net, output, x, y, hard_pairs))
     h = torch.stack(h, dim=0).mean(dim=0).to(device)
-    # print(f"{h=} before abs + 1")
-    # print(f"{(h < 0).sum()} negative values")
     h = torch.abs(h) + 1
-    # print(f"{h=}")
 
     mu_q = parameters_to_vector(net.linear.parameters())
     # mu_q = parameters_to_vector(net.parameters())
     sigma_q = 1 / (h + 1e-6)
-    # print(f"{sigma_q=}")
 
     def sample(parameters, posterior_scale, n_samples=16):
         n_params = len(parameters)
@@ -153,10 +171,12 @@ def run():
     samples = samples.to(device)
 
     logging.info("Generating predictions from samples.")
-    preds = generate_predictions_from_samples(data.test_dataloader(), samples, net, net.linear, device)
+    preds = generate_predictions_from_samples(
+        data.test_dataloader(), samples, net, net.linear, device
+    )
     preds = preds.detach().cpu()
 
-    with open("preds.pkl", "wb") as f:
+    with open("preds_posthoc_25.pkl", "wb") as f:
         pickle.dump(
             {"means": preds.mean(dim=0), "vars": preds.var(dim=0)},
             f,
@@ -164,28 +184,54 @@ def run():
 
     logging.info("Generating ood predictions from samples.")
 
-    ood_data = CIFAR100DataModule("data/", 16, 4)
-    ood_data.setup()
-    test_set = ood_data.dataset_test
-    subset_classes = [0, 1, 2, 3, 4, 5, 6, 7, 8, 9]
-    mask = torch.tensor([test_set[i][1] in subset_classes for i in range(len(test_set))])
-    indices = torch.arange(len(test_set))[mask]
-    ood_dataset = Subset(test_set, indices)
-    ood_dataloader = DataLoader(ood_dataset, batch_size=16, num_workers=4)
-    preds_ood = generate_predictions_from_samples(ood_dataloader, samples, net, net.linear, device)
-    preds_ood = preds_ood.detach().cpu()
+    if ood == "cifar100":
+        ood_data = CIFAR100DataModule("data/", 16, 4)
+        ood_data.setup()
+        test_set = ood_data.dataset_test
+        subset_classes = [0, 1, 2, 3, 4, 5, 6, 7, 8, 9]
+        mask = torch.tensor(
+            [test_set[i][1] in subset_classes for i in range(len(test_set))]
+        )
+        indices = torch.arange(len(test_set))[mask]
+        ood_dataset = Subset(test_set, indices)
+        ood_dataloader = DataLoader(ood_dataset, batch_size=16, num_workers=4)
+        preds_ood = generate_predictions_from_samples(
+            ood_dataloader, samples, net, net.linear, device
+        )
+        preds_ood = preds_ood.detach().cpu()
+    elif ood == "mnist":
+        ood_data = MNISTDataModule("data/", 16, 4)
+        ood_data.setup()
+        preds_ood = generate_predictions_from_samples(
+            ood_data.test_dataloader(), samples, net, net.linear, device
+        )
+        preds_ood = preds_ood.detach().cpu()
+    elif ood == "svhn":
+        ood_data = SVHNDataModule("data/", 16, 4)
+        ood_data.prepare_data()
+        ood_data.setup()
+        preds_ood = generate_predictions_from_samples(
+            ood_data.test_dataloader(), samples, net, net.linear, device
+        )
+        preds_ood = preds_ood.detach().cpu()
+    elif ood == "noise":
+        preds_ood = generate_fake_predictions_from_samples(
+            data.test_dataloader(), samples, net, net.linear, device
+        )
+        preds_ood = preds_ood.detach().cpu()
+    else:
+        raise NotImplementedError
 
-    # preds_ood = generate_fake_predictions_from_samples(data.test_dataloader(), samples, net, net.linear, device)
-    # preds_ood = preds_ood.detach().cpu()
-
-    with open("preds_ood.pkl", "wb") as f:
+    with open("preds_ood_posthoc_25.pkl", "wb") as f:
         pickle.dump(
             {"means": preds_ood.mean(dim=0), "vars": preds_ood.var(dim=0)},
             f,
         )
 
 
-def generate_predictions_from_samples(loader, weight_samples, full_model, inference_model=None, device="cpu"):
+def generate_predictions_from_samples(
+    loader, weight_samples, full_model, inference_model=None, device="cpu"
+):
     if inference_model is None:
         inference_model = full_model
 
@@ -201,7 +247,9 @@ def generate_predictions_from_samples(loader, weight_samples, full_model, infere
     return torch.stack(preds, dim=0)
 
 
-def generate_fake_predictions_from_samples(loader, weight_samples, full_model, inference_model=None, device="cpu"):
+def generate_fake_predictions_from_samples(
+    loader, weight_samples, full_model, inference_model=None, device="cpu"
+):
     if inference_model is None:
         inference_model = full_model
 
